e32dc87be1b390d68423359b578ab2724df75c06
Karri Rasinmäki <karri.rasinmaki@gmail.com>
2015-11-11 18:06:35 +0200

    Initial

---
+++ rvrmth-post-banner.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Plugin Name: Rivermouth Banners
+Plugin URI:  http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Custom post type for banners
+Version:     0.1
+Author:      Rivermouth Ltd
+Author URI:  http://rivermouth.fi
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path: /languages
+Text Domain: rivermouth-post-banner
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+class Rvrmth_PostBanner 
+{
+	
+	public function __construct() 
+	{
+		add_action('init', array($this, 'create_post_type_banner'));
+	}
+
+	public function create_post_type_banner() 
+	{
+		register_post_type('banner', array(
+			'labels' => array(
+				'name' => __( 'Banners', 'rivermouth-post-banner' ),
+				'singular_name' => __( 'Banner', 'rivermouth-post-banner' )
+			),
+			'public' => true,
+			'has_archive' => true,
+			'menu_icon' => 'dashicons-slides',
+			'menu_position' => 20,
+			'supports' => array('title', 'editor', 'thumbnail')
+		));
+	}
+	
+}
+
+new Rvrmth_PostBanner();
+
+?>
+
